3943653ade95a70beafda1497300127ce1bd01af
Ryan Gasparini <ryan@rxgx.com>
2017-10-30 02:57:54 -0700

    Added classes to the markup for forecast elements

--- public/js/app.js
+++ public/js/app.js
@@ -34,7 +34,12 @@ Vue.component('mtb-city', {
     <section class="info">
         <div v-bind:class="'wu wu-64 wu-black wu-' + item.icon"></div>
         <p>{{item.conditions}}</p>
-        <p>High: {{item.high}}&deg; Low {{item.low}}&deg;</p>
+        <p class="mtb-forecast">
+          <span class="mtb-forecast-label">High</span>
+          <span class="mtb-forecast-value">{{item.high}}&deg;</span>
+          <span class="mtb-forecast-label">Low</span>
+          <span class="mtb-forecast-value">{{item.low}}&deg;</span>
+        </p>
         <p>Sunrise: {{item.sunrise}}</p>
         <p>Sunset: {{item.sunset}}</p>
     </section>
